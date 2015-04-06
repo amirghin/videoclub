c21ffc4b968a3b2a58f1c960694f0fa102970b05
Fabiola <fab.tames@gmail.com>
2015-04-05 18:18:50 -0600

    actualizaciones

---
+++ pagina_inicio.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/estilos.css">
+	<script type="text/javascript" src="js/dropdown-menu.js"></script>		
+</head>
+<body>
+	<?php include 'menu.html';?>
+</body>
+</html>
