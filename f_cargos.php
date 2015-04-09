19b2f7ca5c407b076cce70800ce2536255deb8b7
Fabiola <fab.tames@gmail.com>
2015-04-08 18:08:29 -0600

    Agregando formularios

---
+++ f_cargos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insertar Cargos</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+</head>
+<body>
+    <section class="estilos_form">
+		<form action="" method="POST" >
+			<fieldset>
+               <div class="filas">
+                   <label for="detalle">Detalle: </label>
+                   <input type="text" name="detalle">
+               </div>
+               <div class="filas">
+                   <label for="monto">Monto</label>
+                   <input type="text" name="monto">
+               </div>
+               <div class="filas">
+                   <label for="tipo">Tipo</label>
+                   <input type="text" name="tipo">                  
+               </div>
+               <div class="filas">
+                    <input type="submit" class="button" value="Insertar">
+               </div>
+            </fieldset>
+        </form>
+    </section>
+</body>
