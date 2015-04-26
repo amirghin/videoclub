23dbd65a26bc0c88bce4e8c969abf491ec740421
amirghin <adrian@aastorga.com>
2015-04-25 18:18:11 -0600

    modificando peliculas_controller

--- controllers/peliculas_controller.php
+++ controllers/peliculas_controller.php
@@ -1,44 +1,15 @@
 <?php
 
-$mensaje = "";
-$id_pelicula = "";
-$nombre_pelicula = "";
-$precio_alquiler = "";
-$genero = "";
-$duracion = "";
-$ruta_imagenes = "";
+require "../conexion.php";
+require "../classes/peliculas.php";
+$peliculas = new peliculas;
 
-require_once "conexion.php";
-require_once "class/peliculas.php";
-
-if ((isset($_POST['ruta_imagenes']))){
-	echo($_POST['ruta_imagenes']);
-	include_once "ftpupload.php";
-}
-
-
-if(isset($_POST["id_pelicula"], $_POST["nombre_pelicula"], $_POST["precio_alquiler"], $_POST["genero"],  $_POST["duracion"])){
-	try{
-		$peliculas = new peliculas;
-		$id_pelicula = $_POST["id_pelicula"];
-		$nombre_pelicula = $_POST["nombre_pelicula"];
-		$precio_alquiler = $_POST["precio_alquiler"];
-		$genero = $_POST["genero"];
-		$ruta_imagenes = ($_FILES["ruta_imagenes"]["name"]);
-		$duracion = $_POST["duracion"];
-		$peliculas->insertar_peliculas($id_pelicula,$nombre_pelicula,$precio_alquiler,$genero,$ruta_imagenes,$duracion,$conexion);	
-	}catch (Exception $e){
-			//$mensaje = $e->GetMessage();
-		   echo json_encode(array(
-		        'error' => array(
-		            'code' => $e->getCode(),
-		            'message' => $e->getMessage()
-		        )
-		    ));
-
-
-	}
-
+if(isset($_POST["id_pelicula"], $_POST["nombre_pelicula"], $_POST["precio_alquiler"], $_POST["genero"], $_POST["ruta_imagenes"], 
+		$_POST["duracion"])){		
+		$peliculas->insertar_peliculas($_POST["id_pelicula"], $_POST["nombre_pelicula"], $_POST["precio_alquiler"], $_POST["genero"], 
+										$_POST["ruta_imagenes"], $_POST["duracion"], $conexion);	
+}elseif(isset($_POST["id_pelicula"])){
+	$peliculas->buscar_pelicula($_POST["id_pelicula"], $conexion);
 }
 
 ?>
