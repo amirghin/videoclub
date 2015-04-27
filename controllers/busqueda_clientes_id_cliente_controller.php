0e300f861c761a0f32b17cd7f3c7fb7dc3680560
amirghin <adrian@aastorga.com>
2015-04-26 21:12:24 -0600

    busqueda clientes por ID

---
+++ controllers/busqueda_clientes_id_cliente_controller.php
@@ -0,0 +1,14 @@
+<?php
+
+require "../conexion.php";
+require "../classes/clientes.php";
+$cliente = new clientes;
+
+;
+
+if(isset($_POST["id_cliente"])){		
+		$cliente->buscar_cliente($_POST["id_cliente"], $conexion);	
+}
+
+
+?>
