1a87498d08676098c5d71686fece6fe2409a0d2f
Fabiola <fab.tames@gmail.com>
2015-04-27 01:18:43 -0600

    insertar ubicaciones

--- i_ubicaciones.php
+++ i_ubicaciones.php
@@ -1,1 +1,30 @@
-i_ubicaciones.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insertar copias</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <script src="js/jquery-2.1.3.min.js"></script>
+
+</head>
+<body>
+    <section class="estilos_form">
+		<form action="" method="POST" >
+			<fieldset>
+               <div class="filas">
+                   <label for="cod_ubicacion">Codigo de ubicacion: </label>
+                   <input type="text" name="cod_ubicacion"> 
+               </div>
+               <div class="filas">
+                   <label for="detalle">Detalle:</label>
+                   <input type="text" name="detalle">
+               </div>
+               <div class="filas">
+                    <input type="button" class="button" value="Insertar" id="insertar_ubicacion">
+               </div>
+            </fieldset>
+        </form>
+    </section>
+    <script src="js/videoclub.js"></script>
+
+</body>
+</html>
