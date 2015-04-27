7dd4d1774118176a9f3dde6031f71baf27a63682
amirghin <adrian@aastorga.com>
2015-04-26 21:14:36 -0600

    deleting ;

--- controllers/busqueda_clientes_id_cliente_controller.php
+++ controllers/busqueda_clientes_id_cliente_controller.php
@@ -4,8 +4,6 @@ require "../conexion.php";
 require "../classes/clientes.php";
 $cliente = new clientes;
 
-;
-
 if(isset($_POST["id_cliente"])){		
 		$cliente->buscar_cliente($_POST["id_cliente"], $conexion);	
 }
