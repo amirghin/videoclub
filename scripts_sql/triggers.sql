0ab7d82099264e48511a40002beafec96b5a8b34
amirghin <adrian@aastorga.com>
2015-04-26 21:01:45 -0600

    agregando mas triggers

--- scripts_sql/triggers.sql
+++ scripts_sql/triggers.sql
@@ -8,4 +8,41 @@ BEGIN
 
 END; //
 
+DELIMITER //
+CREATE TRIGGER cancelar_reservaciones
+BEFORE UPDATE ON reservaciones
+FOR EACH ROW
+BEGIN
+	DECLARE done INT DEFAULT FALSE;
+    DECLARE ids INT;
+    DECLARE cur CURSOR FOR SELECT copias_id_copia FROM videoclub.peliculas_reservacion WHERE reservaciones_id_reservacion = NEW.id_reservacion;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+	IF NEW.estado_aprobacion = 'rechazado' THEN
+		OPEN cur;
+			up_loop: LOOP
+				FETCH cur INTO ids;
+				IF done THEN
+					LEAVE up_loop;
+				END IF;
+				UPDATE videoclub.copias SET disponibilidad= 'disponible' WHERE id_copia = ids;
+			END LOOP;
+		CLOSE cur;
+	END IF;
+        
+END; //
+
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER liberar_copias
+BEFORE DELETE ON peliculas_reservacion
+FOR EACH ROW
+BEGIN
+	
+    UPDATE videoclub.copias SET disponibilidad= 'disponible' WHERE id_copia = OLD.copias_id_copia;
+
+END; //
+
 DELIMITER ;
