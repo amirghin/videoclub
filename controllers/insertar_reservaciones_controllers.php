0ede5db1dcf131d1afd6ff83f2a655f2b1b74997
Fabiola <fab.tames@gmail.com>
2015-04-27 21:21:50 -0600

    merge en reservaciones controller

--- controllers/insertar_reservaciones_controllers.php
+++ controllers/insertar_reservaciones_controllers.php
@@ -4,15 +4,10 @@ require "../conexion.php";
 require "../classes/reservaciones.php";
 require "../classes/peliculas_reservacion.php";
 $reservacion = new reservaciones;
-<<<<<<< Updated upstream
-session_start();
-if(isset($_POST["id_cliente"], $_POST["estado_aprobacion"], $_POST["fecha_entrega"], $_POST["fecha_reservacion"], $_POST["fecha_retiro"], $_POST["hora_retiro"])){		
-		$reservacion->insertar_reservacion($_POST["id_cliente"], $_POST["estado_aprobacion"], $_POST["fecha_entrega"], $_POST["fecha_reservacion"], $_POST["fecha_retiro"], $_POST["hora_retiro"], $conexion);	
-=======
 
 if(isset($_POST["id_cliente"], $_POST["estado_aprobacion"], $_POST["id_copia"], $_POST["fecha_entrega"], $_POST["fecha_reservacion"], $_POST["fecha_retiro"], $_POST["hora_retiro"])){		
 		$reservacion->insertar_reservacion($_POST["id_cliente"], $_POST["id_copia"],$_POST["estado_aprobacion"],$_POST["fecha_entrega"], $_POST["fecha_reservacion"], $_POST["fecha_retiro"], $_POST["hora_retiro"], $conexion);	
->>>>>>> Stashed changes
+
 }
 
 ?>
