63410d75f69efab4e9fd52ca5194f0977f5f55e3
Amirghin <adrian@aastorga.com>
2015-04-28 11:46:23 -0700

    prueba pantalla de inicio

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
 unset($_SESSION["usuario"]);
-unset($_SESSION["id_usuario"]);
+unset($_SESSION["id_usuario"]); 
+unset($_SESSION["nombre_usuario"]); 
+header ("Location: login.php");
 ?>
--- pagina_inicio.php
+++ pagina_inicio.php
@@ -1,15 +1,43 @@
-<?php
-session_start();
-?>
-
+<?php session_start();?>
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="css/estilos.css">
-	<script type="text/javascript" src="js/dropdown-menu.js"></script>		
+	<title>Sistema de Manejo de VideoClub</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+    <!--<link rel="stylesheet" type="text/css" href="css/estilos.css">-->
+
 </head>
+
 <body>
-	<?php include 'menu.html';?>
-	<p align="right">Bienvenido <?php echo $_SESSION["nombre_usuario"]?></p>
+	<nav id="top-nav" class="navbar navbar-inverse navbar-static-top">
+
+		<div class="container-fluid">
+			<div class="navbar-header">
+	          <a class="navbar-brand" href="#">Video Club</a>
+	        </div>
+			<div class="navbar-collapse collapse">
+				<a class="navbar-brand" href="#"><?php echo "Bienvenido ". $_SESSION["nombre_usuario"];?></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<a class="navbar-brand" href="logout.php"> Cerrar Session</a>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-3 col-md-2 sidebar">
+				<h3><i class="glyphicon glyphicon-briefcase"></i> Busqueda peliculas</h3>
+		  		<hr>
+				<ul class="nav nav-stacked">
+					<li><a href="">Buscar peliculas</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main"></div>
+		</div>
+	</div>
 </body>
+
 </html>
+
