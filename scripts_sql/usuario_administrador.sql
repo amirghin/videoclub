a59d095465fd92024c92b6199a5013998cd5ab0e
amirghin <adrian@aastorga.com>
2015-04-27 20:41:08 -0600

    usuarios

--- scripts_sql/usuario_administrador.sql
+++ scripts_sql/usuario_administrador.sql
@@ -1,2 +1,5 @@
-INSERT INTO videoclub.usuarios_administradores (nom_usuario, nombre, apellido, contrasena, usuario_creacion, fecha_creacion, usuario_modificacion, fecha_modificacion, habilitado)
-	VALUES ('admin', 'administrador', 'administrador', '$2y$11$jLbv7LUj0XyAAJ5GFHVwuOi7lvWazUV5bPbhV021eVsS9DVhDx7Xu', 'system', CURDATE(), 'system', CURDATE(), 1);
+INSERT INTO videoclub.usuarios_administradores (id_usuario, nom_usuario, nombre, apellido, contrasena, usuario_creacion, fecha_creacion, usuario_modificacion, fecha_modificacion, habilitado)
+	VALUES (1, 'admin', 'administrador', 'administrador', '$2y$11$jLbv7LUj0XyAAJ5GFHVwuOi7lvWazUV5bPbhV021eVsS9DVhDx7Xu', 'system', CURDATE(), 'system', CURDATE(), 1);
+
+INSERT INTO videoclub.usuarios_administradores (id_usuario, nom_usuario, nombre, apellido, contrasena, usuario_creacion, fecha_creacion, usuario_modificacion, fecha_modificacion, habilitado)
+	VALUES (2, 'system_web', 'system_web', 'system_web', '$2y$11$jLbv7LUj0XyAAJ5GFHVwuOi7lvWazUV5bPbhV021eVsS9DVhDx7Xu', 'system', CURDATE(), 'system', CURDATE(), 0);
